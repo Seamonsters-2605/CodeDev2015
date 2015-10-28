caa3980c87bd5c379f81bbb5ac5bcf92ed7a801c
Ian <ianproth@gmail.com>
2015-10-27 18:44:53 -0700

    Add some spacing , use __init__ instead

--- Dawson/src/Buttercraft/robot.py
+++ Dawson/src/Buttercraft/robot.py
@@ -12,23 +12,29 @@ class Buttercraft(wpilib.IterativeRobot):
         self.Configurer = CanTalonConfig.CanTalonConfig(wpilib.CANTalon.ControlMode.Speed, wpilib.CANTalon.FeedbackDevice.QuadEncoder)
         self.Configurer.setRecommendedPIDF()
         self.Configurer.setBrakeMode(True)
+        
         self.FL = wpilib.CANTalon(51)#test these numbers
         self.FR = wpilib.CANTalon(52)
         self.BL = wpilib.CANTalon(53)
         self.BR = wpilib.CANTalon(54)
+        
         self.Lift = wpilib.CANTalon(41)
+        
         self.FR.reverseSensor(flip=-1)
         self.BR.reverseSensor(flip=-1)
+        
         self.Configurer.config(self.FL)
         self.Configurer.config(self.FR)
         self.Configurer.config(self.BL)
         self.Configurer.config(self.BR)
+        
         self.PDP = wpilib.PowerDistributionPanel()
         self.DB = wpilib.SmartDashboard()
+        
         self.StrafeJoystick = wpilib.Joystick(1)
         self.TurnJoystick = wpilib.Joystick(0)
-        self.PR = PowerReporter.PowerReporter()
-        self.PR.setUp(self.PDP, self.DB)
+        
+        self.PR = PowerReporter.PowerReporter(self.PDP, self.DB)
         self.PR.setDefaultPortNames()
         #USE THESE
         self.Joystick = Joysticks.Joysticks(self.StrafeJoystick, self.TurnJoystick, .1)
